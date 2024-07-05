7ec49f30ec8cd1bb9ee31856f9e1f1f4a8c08c17
cccoodde <135531276+cccoodde@users.noreply.github.com>
2024-07-05 16:36:56 +0900

    커밋

--- src/main/java/org/hj/controller/MapFetchController.java
+++ src/main/java/org/hj/controller/MapFetchController.java
@@ -1,9 +1,13 @@
 package org.hj.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.hj.model.GPSInfoVO;
 import org.hj.service.GPSInfoService;
+import org.hj.service.NurseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class MapFetchController {
 	
 	@Autowired
-	GPSInfoService gs;
+	NurseService ns;
 	
 	
-	// 위도 경도 저장 컨트롤러
-	@PostMapping("/sendLatLon")
-    public String sendLatLon(@RequestBody GPSInfoVO gvo) {
-		gs.sendLatLon(gvo);
-        return "Data saved successfully";
-    }
+	// 사용자 좌표 위치 DB에 넣기
+		@PostMapping(value = "/sendLatLon", produces = "application/json")
+		public String goEx(@RequestBody GPSInfoVO gvo, HttpSession session) {
+		
+			String id = (String)(session.getAttribute("userId"));
+			gvo.setId(id);				
+			ns.callInsertEx(gvo);
+			System.out.println(gvo);
+			return "성공 적으로 데이터 전송 성공";
+		}
+		
+		// DB 사용자 좌표 가져오기
+		@GetMapping(value = "/u_position", produces = "application/json")
+		public GPSInfoVO getGps(HttpSession session,GPSInfoVO gvo) {
+			System.out.println("좌표가져오기");
+			String id = (String)(session.getAttribute("userId"));
+			gvo.setId(id);
+			System.out.println(ns.getGps(gvo));
+			return ns.getGps(gvo);
+		}
 }
